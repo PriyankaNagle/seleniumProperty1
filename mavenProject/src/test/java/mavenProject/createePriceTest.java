bfa25a5c67a80e612f30e4b7f36df71bb2104079
PRIYANKA <PRIYANKA@LAPTOP-98SB48LL.mshome.net>
2026-04-21 14:35:17 +0530

    Initial commit1

---
+++ mavenProject/src/test/java/mavenProject/createePriceTest.java
@@ -0,0 +1,18 @@
+package mavenProject;
+
+import org.testng.annotations.Test;
+
+public class createePriceTest {
+	@Test
+	public void createPrice() {
+		System.out.println("Execute Pricecontact Test");
+	}
+	
+	@Test
+	public void modifyPrice() {
+		System.out.println("Execute modifycontact Test");
+		
+	}
+
+
+}
---
+++ mavenProject/src/test/java/mavenProject/createeSellTest.java
@@ -0,0 +1,15 @@
+package mavenProject;
+
+import org.testng.annotations.Test;
+
+public class createeSellTest {
+	@Test
+	public void createSellTest() {
+		System.out.println("execute createSellTest");
+	}
+	@Test
+	public void modifySellTest() {
+		System.out.println("execute modifySellTest");
+	}
+
+}
